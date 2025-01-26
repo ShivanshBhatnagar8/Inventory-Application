d5e9997abfa17867fc7b84dd3a3b6b910b76c262
Shivansh Bhatnagar <113698150+ShivanshBhatnagar8@users.noreply.github.com>
2025-01-26 13:15:30 +0530

    removed commented code

--- routes/Authors.js
+++ routes/Authors.js
@@ -4,12 +4,4 @@ const { getAuthors } = require("../controllers/AuthorController");
 
 routes.get("/", getAuthors);
 
-//routes.get("/", getUser);
-// routes.get("/new", createUsernameGet);
-
-// routes.post("/new", createUsernamePost);
-
-// routes.get("/ok", deleteNullUsers);
-
-// routes.get("/delete", deleteAllUsers);
 module.exports = routes;
