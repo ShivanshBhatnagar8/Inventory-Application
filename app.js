08dd60db7e25a1fb39ff1c5314636dc5fa77ddbf
shivansh bhatnagar <shivanshbhatnagar8@gmail.com>
2025-01-26 13:12:16 +0530

    created functionality to make app work

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const path = require("node:path");
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const authorRoutes = require("./routes/Authors");
+const bookRoutes = require("./routes/Books");
+const categoryRoutes = require("./routes/Categories");
+
+app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/", bookRoutes);
+app.use("/authors", authorRoutes);
+app.use("/category", categoryRoutes);
+app.listen(3000);
